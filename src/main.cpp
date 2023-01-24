fad4142bb147a7fc17c87d168eb6a1fb7f76f904
Cai Heran <Tsai.love.dev@outlook.com>
2023-01-24 17:55:21 +0800

    Made some small optimizations.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
  * @brief Search the given directory for duplicate regular files.
  *
  * The standard output will look like:
-以下是空文件：
+Empty file list:
 ...
 
-空文件：xx
-总文件：xxx
+Empty: xx
+Total: xxx
 
- #1 (n1) xxxB
+ #1 (n1) xxx B
 ...
 ...
 
- #2 (n2) xxxxB
+ #2 (n2) x xxx B
 ...
 ......
 
@@ -31,11 +31,11 @@ Done in x.xxxs.
 
 #include "xxhash.hpp"
 
-using std::format;
-
 namespace fs = std::filesystem;
 namespace ranges = std::ranges;
 namespace views = std::views;
+
+using std::format;
 
 #ifdef __WINDOWS__
 # define myout std::wcout
@@ -47,8 +47,8 @@ namespace views = std::views;
 
 
 /**
- * @brief Hash all @p files and sort them by hash value.
- * 
+ * @brief Group the same files in @p filelist into @p res.
+ *
  * Algorithm:
  * In case of small files, directly hash the whole files.
  * Or else, hash the first 512 B and the last 512 B firstly,
@@ -61,17 +61,16 @@ template<ranges::input_range Iterable, class Container>
 void hash_check(Iterable&& filelist, Container &res)
 {
     constexpr std::size_t buffersize{1<<15}; // 32 KiB
-    xxh::hash3_state128_t state;
     std::map<xxh::hash128_t, typename Container::value_type> map1, map2;
     char buffer[buffersize];
 
     if (fs::file_size(*filelist.cbegin()) <= buffersize)
     {
-        for (auto& file: filelist) {
+        for (auto&& file: filelist) {
             std::ifstream fin(file, std::ios_base::binary);
             fin.read(buffer, buffersize);
             auto hash = xxh::xxhash3<128>(buffer, fin.gcount());
-            map1[hash].emplace_back(std::move(file));
+            map1[hash].emplace_back(std::forward_like<Iterable>(file));
         }
         for (auto& files: map1 | views::values)
             if (files.size() > 1)
@@ -87,12 +86,13 @@ void hash_check(Iterable&& filelist, Container &res)
         fin.seekg(-halfsize, std::ios_base::end);
         fin.read(buffer+halfsize, halfsize);
         auto hash = xxh::xxhash3<128>(buffer, buffersize);
-        map1[hash].emplace_back(std::move(file));
+        map1[hash].emplace_back(std::forward_like<Iterable>(file));
     }
+
+    xxh::hash3_state128_t state;
 
     for (auto& files1: map1 | views::values)
     if (files1.size() > 1) {
-        map2.clear();
         for (auto& file: files1) {
             std::ifstream fin(file, std::ios_base::binary);
             state.reset();
@@ -103,6 +103,7 @@ void hash_check(Iterable&& filelist, Container &res)
         for (auto& files2: map2 | views::values)
             if (files2.size() > 1)
                 res.emplace_back(std::move_if_noexcept(files2));
+        map2.clear();
     }
 }
 
@@ -112,33 +113,33 @@ void hash_check(Iterable&& filelist, Container &res)
  * @return a pair of the numbers of non-empty files ans all regular files.
  */
 template <class Container>
-auto search(const fs::path dir, Container& size_map)
+auto search(const fs::path& dir, Container& size_map)
 {
     std::size_t tot=0, empty=0;
 
-    myout << _T("以下是空文件：\n");
-    for (const auto& entry: fs::recursive_directory_iterator(dir))
+    myout << _T("Empty file list:") << std::endl;
+    for (const auto& entry: fs::recursive_directory_iterator{dir})
     if (entry.is_regular_file())
     {
         tot++;
         auto& path_str = entry.path().native();
-        if (!entry.file_size()) {
+        if (entry.file_size()) {
+            size_map[entry.file_size()].emplace_back(path_str);
+        }
+        else {
             empty++;
             myout << path_str << std::endl;
         }
-        else {
-            size_map[entry.file_size()].emplace_back(path_str);
-        }
     }
 
-    myout << format(_T("\n空文件：{}\n总文件：{}\n"), empty, tot) << std::endl;
+    myout << format(_T("\nEmpty: {}\nTotal: {}\n"), empty, tot) << std::endl;
 
     return std::make_pair(tot-empty, tot);
 }
 
 /**
  * @brief Search @p dirpath for duplicate files.
- * 
+ *
  * Algorithm:
  * 1. Search @p dirpath recursively for all regular files.
  * 2. Group files by size, with empty files directly output.
@@ -152,6 +153,7 @@ void duplicate_file_search(const fs::path dirpath)
     std::map<std::uint64_t, std::vector<path_string>> size_map;
     search(dirpath, size_map);
     std::size_t num=0;
+    std::uint_fast64_t rdsize=0; // redundant data size
 
     for (auto&& pair: size_map)
     {
@@ -164,22 +166,22 @@ void duplicate_file_search(const fs::path dirpath)
         hash_check(std::move(paths), res);
         for (auto&& paths: res) {
             num++;
+            rdsize += filesize * (paths.size()-1);
             ranges::sort(paths);
-            myout << format(_T(" #{} ({}) {}B\n"), num, paths.size(), filesize);
+            myout << format(_T(" #{} ({}) {:L} B\n"), num, paths.size(), filesize);
             for (const auto& p: paths)
                 (myout << p).put('\n');
             myout << std::endl;
         }
     }
 
-    myout << format(_T("Done in {}s.\n"), (double)clock()/CLOCKS_PER_SEC);
+    myout << format(_T("Redundant data size: {:L} B\n\nDone in {:.3f}s.\n"),
+                    rdsize, (double)clock()/CLOCKS_PER_SEC);
 }
 
 int main(int argc, char *argv[])
 {
-    std::locale::global(std::locale("zh_CN"));
-    myout.imbue(std::locale("zh_CN"));
-
+    std::locale::global(std::locale{""});
     std::ios_base::sync_with_stdio(false);
 
     fs::path dir;
